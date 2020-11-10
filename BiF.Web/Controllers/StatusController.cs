025aa5dd64360625ac12efe1a6622d3040a56691
Brad Dawgert <brad_dawgert@ao.uscourts.gov>
2020-11-10 11:50:17 -0500

    Add more DAL references to the Status page for keep warm.

--- BiF.Web/Controllers/StatusController.cs
+++ BiF.Web/Controllers/StatusController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web.Mvc;
 
 namespace BiF.Web.Controllers
@@ -12,11 +10,16 @@ namespace BiF.Web.Controllers
 
             ViewBag.PageTitle = "Status";
 
-            int userCount = DAL.Context.Users.Count(x => x.UserStatus >= 0);
-            List<string> exchanges = DAL.Context.Exchanges.Select(x => x.Name).ToList();
+            var users = DAL.Context.Users.Select(x => new { x.Email, x.Profile.RedditUsername, x.UserStatus }).ToList();
+            var exchanges = DAL.Context.Exchanges.Select(x => new { x.Name, x.OpenDate, x.SignUps }).ToList();
+
+            int userCount = users.Count(x => x.UserStatus > 0);
+            int pendingCount = users.Count(x => x.UserStatus == 0);
 
             ViewBag.Message =
-                $"<p>OK.</p><p>{userCount} Users.</p><p>Exchanges: {string.Join("; " + Environment.NewLine, exchanges)}</p>";
+                "<p>Status: OK</p>" + 
+                $"<p>{userCount} Users ({pendingCount} Pending).</p>" + 
+                $"<p>Exchanges: {string.Concat(exchanges.Select(x => $"<div>{x.Name} ({x.OpenDate.ToShortDateString()}): {x.SignUps.Count} signups</div>"))}</p>";
 
             return View("Message");
         }
