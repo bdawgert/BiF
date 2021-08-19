769fc9af8a8a78c4e1a3b058103f78170b7215c5
Brad Dawgert <brad_dawgert@ao.uscourts.gov>
2021-08-19 03:14:08 -0400

    Changes for responsiveness

--- BiF.Web/Content/js/tools.js
+++ BiF.Web/Content/js/tools.js
@@ -1,4 +1,22 @@
 ﻿
+$(function() {
+
+	$('.navigation-hamberder').on('click', function (e) {
+		console.log('click');
+		e.stopPropagation();
+		$('.navigation-bar').toggle();
+	});
+
+	$(document).on('click', function (e) {
+		console.log('click');
+		e.stopPropagation();
+
+		if ($('.navigation-hamberder').is(':visible') && $('.navigation-bar').is(':visible'))
+			$('.navigation-bar').hide();
+	});
+});
+
+
 var Popup = function() {
 	this._popup = $('.popup');
 	this.Show = function(msg) {
